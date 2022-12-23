7e392347daf9f65703856fdf93a9b726a3117951
Dominik Skrzydlewski <skrzydlewskijunior@gmail.com>
2022-12-23 23:03:07 +0100

    Dodanie obsługi czasu do gry oraz restartu

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -15,37 +15,90 @@ using System.Windows.Shapes;
 
 namespace MatchGame
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
-
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Jeszcze raz?";
+            }
         }
 
         private void SetUpGame()
         {
             List<string> animalEmoji = new List<string>()
             {
-                "🐙", "🐙",
+                "🐙","🐙",
                 "🐟","🐟",
                 "🐕","🐕",
-                "🦘","🐅",
-                "🐪","🐅",
-                "🐒","🐅",
-                "🦓","🐅",
+                "🦘","🦘",
+                "🐪","🐪",
+                "🐒","🐒",
+                "🦓","🦓",
                 "🐅","🐅",
             };
             Random random = new Random();
-            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) { 
-                int index = random.Next(animalEmoji.Count);
-                string nextEmoji = animalEmoji[index];
-                textBlock.Text= nextEmoji;
-                animalEmoji.RemoveAt(index);
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) {
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count);
+                    string nextEmoji = animalEmoji[index];
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
+            }
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if(findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }else if(textBlock.Text == lastTextBlockClicked.Text){
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
             }
         }
     }
